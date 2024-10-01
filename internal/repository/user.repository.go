104c123fccbfb79a4fd52c445e41f492c540e157
Denta Muhajir <dentamuhajir@gmail.com>
2024-10-01 21:12:08 +0700

    Add repository user

---
+++ internal/repository/user.repository.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/dentamuhajir/backend-service-go-mysql/internal/model"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db}
+}
+
+func (r UserRepository) GetUser() ([]model.User, error) {
+	query := "SELECT * FROM user"
+	rows, err := r.db.Query(query)
+	if err !=nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []model.User
+	for rows.Next() {
+		var user model.User
+		err := rows.Scan(
+			&user.ID, 
+			&user.Username,
+			&user.Password,
+			&user.Email,
+		)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
