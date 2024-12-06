76c18fc6e9ea4d38488c493ddb0077a6b7913f58
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-06 13:52:45 +0700

    Create service for article

---
+++ internal/service/article.service.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"github.com/dentamuhajir/backend-service-go-mysql/internal/model"
+	"github.com/dentamuhajir/backend-service-go-mysql/internal/repository"
+)
+
+type ArticleService struct {
+	articleRepository repository.ArticleRepository
+}
+
+func NewArticleService(r repository.ArticleRepository) *ArticleService {
+	return &ArticleService{
+		articleRepository: r,
+	}
+}
+
+func (s ArticleService) GetListArticle() ([] model.Article, error) {
+	return s.articleRepository.GetArticleList()
+}
