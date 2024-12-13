47de9cd7bf2cd13ae020b0c3e39c4d8d5960c780
Denta Muhajir <dentamuhajir@gmail.com>
2024-12-14 00:58:18 +0700

    Update repository with pointer receivers for performance & consistency

--- internal/repository/article.repository.go
+++ internal/repository/article.repository.go
@@ -14,7 +14,7 @@ func NewArticleRepository(db *sql.DB) *ArticleRepository {
 	return &ArticleRepository{db: db}
 }
 
-func (r ArticleRepository) GetHeadlineArticle() ([]model.Article, error) {
+func (r *ArticleRepository) GetHeadlineArticle() ([]model.Article, error) {
 	query := "SELECT id, title, author, category, content_body, photo, photographer, published_date FROM articles ORDER BY RAND() LIMIT 2"
 	rows, err := r.db.Query(query)
 	if err != nil {
@@ -43,7 +43,39 @@ func (r ArticleRepository) GetHeadlineArticle() ([]model.Article, error) {
 	return articles, nil
 }
 
-func (r ArticleRepository) GetArticleList() ([]model.Article, error) {
+func (r *ArticleRepository) GetDetailArticle() ([]model.Article, error) {
+	query := "SELECT id, title, author, category, content_body, photo, photographer, published_date FROM articles"
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var articles []model.Article
+	for rows.Next() {
+		var article model.Article
+		err := rows.Scan(
+			&article.ID,
+			&article.Title,
+			&article.Author,
+			&article.Category,
+			&article.ContentBody,
+			&article.Photo,
+			&article.Photographer,
+			&article.PublishedDate,
+			//&article.IsPublished,
+			// &article.CreatedAt,
+			// &article.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, article)
+	}
+	return articles, nil
+}
+
+func (r *ArticleRepository) GetArticleList() ([]model.Article, error) {
 	query := "SELECT id, title, author, category, content_body, photo, photographer, published_date FROM articles"
 	rows, err := r.db.Query(query)
 	if err != nil {
